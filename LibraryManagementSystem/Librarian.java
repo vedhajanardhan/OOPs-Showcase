7838738a626c192ac9ca091d5d12c980f8f2b5fd
Vedha Janardhan <vedhajanardhan@gmail.com>
2026-07-09 15:13:09 +0530

    Update Librarian.java

--- LibraryManagementSystem/Librarian.java
+++ LibraryManagementSystem/Librarian.java
@@ -1,1 +1,9 @@
+public class Librarian extends User {
+    public Librarian(String name) { super(name); }
+
+    @Override
+    public void borrowBook(Book book) {
+        System.out.println("Librarian " + name + " manages borrowing of " + book.getTitle());
+    }
+}
 
