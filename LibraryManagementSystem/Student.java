0b069f6af97f250cdf28ba3b8a175fc2f69d9a68
Vedha Janardhan <vedhajanardhan@gmail.com>
2026-07-09 15:13:37 +0530

    Update Student.java

--- LibraryManagementSystem/Student.java
+++ LibraryManagementSystem/Student.java
@@ -1,1 +1,14 @@
+public class Student extends User {
+    public Student(String name) { super(name); }
+
+    @Override
+    public void borrowBook(Book book) {
+        if(book.isAvailable()) {
+            book.borrowBook();
+            System.out.println(name + " borrowed " + book.getTitle());
+        } else {
+            System.out.println(book.getTitle() + " is not available.");
+        }
+    }
+}
 
