45396f3414cff18cbcbbf32c97e57f97ab9135f0
Vedha Janardhan <vedhajanardhan@gmail.com>
2026-07-09 15:12:12 +0530

    Update Main.java

--- LibraryManagementSystem/Main.java
+++ LibraryManagementSystem/Main.java
@@ -1,1 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Library library = new Library();
+        Book b1 = new Book("Java Basics", "James Gosling");
+        Book b2 = new Book("OOP Concepts", "Bjarne Stroustrup");
+
+        library.addBook(b1);
+        library.addBook(b2);
+
+        Student s1 = new Student("Vedha");
+        Librarian l1 = new Librarian("Ravi");
+
+        s1.borrowBook(b1); // Student borrows
+        l1.borrowBook(b2); // Librarian manages
+    }
+}
 
